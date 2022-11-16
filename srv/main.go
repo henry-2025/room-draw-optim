32b307a9c1f5692121a418a35123d02843b2f3f2
Henry Pick <hpick@hmc.edu>
2022-11-16 13:10:39 -0800

    create module

---
+++ srv/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/api/status", getStatus)
+
+	r.Run()
+}
+
+func getStatus(c *gin.Context) {
+	c.JSON
+}
